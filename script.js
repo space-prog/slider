f691775cef2218bbfa9477efe1e06419e8364f40
space-prog <p87717891@gmail.com>
2026-05-16 14:22:18 +0300

    dsf

--- script.js
+++ script.js
@@ -9,11 +9,17 @@ const STEP = SLIDE_W + GAP
 let isAnimating = false
 let direction = null
 
-
-
 function getSlides() {
   return [...track.querySelectorAll('.item')]
 }
+
+const items = getSlides()
+
+let cloneFirst = items[0].cloneNode(true)
+let cloneLast = items[items.length - 1].cloneNode(true)
+
+track.appendChild(cloneFirst, items[items.length - 1])
+track.insertBefore(cloneLast, items[0])
 
 function setPos(vw, animate) {
   track.style.transition = animate
@@ -22,14 +28,14 @@ function setPos(vw, animate) {
   track.style.transform = `translateX(${vw}vw)`
 }
 
-setPos(0, false)
+setPos(-22.5, false)
 
 btnR.addEventListener('click', (e) => {
   e.preventDefault()
   if (isAnimating) return
   isAnimating = true
   direction = 'right'
-  setPos(-STEP, true)
+  setPos(-STEP * 2, true)
 })
 
 btnL.addEventListener('click', (e) => {
@@ -51,7 +57,7 @@ track.addEventListener('transitionend', (e) => {
     track.insertBefore(slides[slides.length - 1], slides[0])
   }
 
-  setPos(0, false)
+  setPos(-22.5, false)
   direction = null
   isAnimating = false
 })
